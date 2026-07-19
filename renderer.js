0477fc214e9017da122f7688f377222e8af6b022
xivmcm <xivmcm@users.noreply.github.com>
2026-07-19 19:49:44 +0700

    Bump version to 1.12.7

--- renderer.js
+++ renderer.js
@@ -1,5 +1,5 @@
 const isElectron = Boolean(window.electronAPI);
-const APP_VERSION = '1.12.6';
+const APP_VERSION = '1.12.7';
 document.body.classList.toggle('electron-runtime', isElectron);
 document.body.classList.toggle('web-runtime', !isElectron);
 
